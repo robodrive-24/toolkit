7c02de865f4bd51ddb819db2c10c13d78f770e3b
Daniel-xsy <1491387884@qq.com>
2024-01-22 14:40:57 -0800

    [Update] Add corruption test script

--- track-5/BEVFusion/tools/test.sh
+++ track-5/BEVFusion/tools/test.sh
@@ -1,1 +1,1 @@
-torchpack dist-run -np 2 python tools/test.py configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml pretrained/bevfusion-det.pth --eval bbox
+torchpack dist-run -np 2 python tools/test.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml pretrained/bevfusion-det.pth --eval bbox
